4f60680258fae435f2c2f0a4927b04fb99164937
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-03-10 02:34:30 +0530

    Completed

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,31 @@
 var swiper = new Swiper(".slide-content ", {
     slidesPerView: 3,
-    spaceBetween: 25,
+    spaceBetween: 20,
     loop: true,
-    centerSlides: true,
-    fade:'true',
-    // gragCursor: true,
-    cssMode: true,
+    centerSlide: true,
+    fade: 'true',
+    gragCursor: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
     },
     mousewheel: true,
     keyboard: true,
-  });
+
+});
